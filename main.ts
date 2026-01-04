3ef45fbbc2f469a4ccfcbdab7ab07d656eab7ad4
paynor <69216839+paynor@users.noreply.github.com>
2026-01-03 23:16:32 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,10 @@
-//radio.onReceivedValueDeprecated(function (name, value) {
-//    if (name == "A") {
-//        arm = value
-//    }
-//    if (name == "R") {
-//        roll = value
-//    }
-//    if (name == "T") {
-//        throttle = value
-//    }
-//})
 let throttle = 0
 let roll = 0
-let arm = 1
-// let radioGroup = 7
-// radio.setGroup(radioGroup)
-// basic.showNumber(radioGroup)
-if (arm) {
-    while (arm) {
-        basic.showIcon(IconNames.No)
-    }
-    basic.showIcon(IconNames.Yes)
-}
-basic.forever(function () {
-//    basic.showNumber(Math.round(throttle / 10))
-})
+let arm = 0
+
 basic.forever(function () {
     if (arm) {
-        hoverbit.start_cushion_simple()
+    //    hoverbit.start_cushion_simple()
         hoverbit.forward_power_simple(throttle)
     } else {
         hoverbit.stop_all_motors()
